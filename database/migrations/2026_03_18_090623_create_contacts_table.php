b7e89e8c7bc8ed3cf5815527a097f8fc16cf7974
kooriyama azuki <hyouzan.azuki.0509@gmail.com>
2026-03-18 19:02:07 +0900

    first commit

---
+++ database/migrations/2026_03_18_090623_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->unsignedBigInteger('category_id');
+            $table->tinyInteger('gender');
+            $table->string('email');
+            $table->string('tel');
+            $table->string('address');
+            $table->string('building');
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
